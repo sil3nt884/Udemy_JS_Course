c462a0ff00e5d85d5213cbd5d37f3a72185de002
ricky.luu <sil3nt884@hotmail.co.uk>
2018-05-17 00:26:48 +0100

    lesson1

---
+++ udemy_js_lesson1/JS Basics/Strings.js
@@ -0,0 +1,13 @@
+/**
+ * Make 3 variables
+ * City, Country Location
+ *
+ * Output : London, UK
+ * @type {string}
+ */
+
+let city = "London";
+let country = "UK";
+let location = city+", "+country;
+
+console.log(location);
